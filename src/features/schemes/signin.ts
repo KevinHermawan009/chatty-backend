86caa0136e8d32fffab003c7ee5d2f8eb9214f51
kevinhermawan009 <reactjugger@gmail.com>
2022-12-09 02:45:41 +0700

    adding signin schema

---
+++ src/features/schemes/signin.ts
@@ -0,0 +1,18 @@
+import Joi, { ObjectSchema } from 'joi';
+
+export const loginSchema: ObjectSchema = Joi.object().keys({
+
+  username: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'username must be a string',
+    'string.min': 'username invalid',
+    'string.max': 'username invalid',
+    'string.empty': 'username must be filled'
+  }),
+  password: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'password must be a string',
+    'string.min': 'password invalid',
+    'string.max': 'password invalid',
+    'string.empty': 'password must be filled'
+  })
+
+});
