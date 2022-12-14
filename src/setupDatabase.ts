268f7fd48fafed0b41a7e0b56501a6ef89d413a0
kevinhermawan009 <reactjugger@gmail.com>
2022-12-15 04:04:56 +0700

    Adding redis connection

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { config } from '@root/config';
 import Logger from 'bunyan';
+import { redisConnection } from '@services/redis/redis.connnection';
 
 const log: Logger = config.createLogger('setupDatabase');
 mongoose.set('strictQuery', false); //remove warning
@@ -8,9 +9,10 @@ mongoose.set('strictQuery', false); //remove warning
 export default () => {
   const connect = () => {
     mongoose
-      .connect(config.DATABASE_URL!)
+      .connect(`${config.DATABASE_URL}`)
       .then(() => {
         log.info('database connected', config.DATABASE_URL);
+        redisConnection.connect();
       })
       .catch((err) => {
         log.error(err);
