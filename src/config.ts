cbbed7c6b4c25b99df4ddefd42295f15b43921b2
kevinhermawan009 <reactjugger@gmail.com>
2022-12-15 03:32:33 +0700

    changing dotenv declaring method

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import bunyan from 'bunyan';
 import cloudinary from 'cloudinary';
+import path from 'path';
 
-dotenv.config({});
+dotenv.config({path: path.resolve(__dirname, '../env/.env')});
+
 class Config {
+
   public DATABASE_URL: string | undefined;
   public JWT_TOKEN: string | undefined;
   public NODE_ENV: string | undefined;
@@ -15,11 +18,10 @@ class Config {
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
 
-  private readonly DEFAULT_DATABASE_URL = 'mongodb+srv://chatty-backend.kikn1pw.mongodb.net/Chatty-Backend'; //setDefaultValue
+  private readonly DEFAULT_DATABASE_URL = 'mongodb+srv://kevin:kevin@chatty-backend.kikn1pw.mongodb.net/?retryWrites=true&w=majority'; //setDefaultValue
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
-    console.log(this.DATABASE_URL);
     this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
     this.NODE_ENV = process.env.NODE_ENV || '';
     this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
@@ -47,6 +49,9 @@ class Config {
     cloud_name : this.CLOUD_NAME,
     api_key : this.CLOUD_API_KEY,
     api_secret : this.CLOUD_API_SECRET
+    // cloud_name : 'dqn9uwp3c',
+    // api_key : '649496521367673',
+    // api_secret : 'anbmr4r5WCecXtlq2WZ59OrI0pM'
   });
   }
 }
