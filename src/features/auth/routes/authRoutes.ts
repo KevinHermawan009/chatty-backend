972f0af31a6cf6a4aaa0fe1e19253ad9c2984213
kevinhermawan009 <reactjugger@gmail.com>
2023-01-09 14:59:58 +0700

    Split signin routes

--- src/features/auth/routes/authRoutes.ts
+++ src/features/auth/routes/authRoutes.ts
@@ -4,23 +4,27 @@ import { SignUp } from '@auth/controllers/signupController';
 import express, { Router } from 'express';
 
 class AuthRoutes {
-    private router: Router;
+  private router: Router;
 
-    constructor(){
-        this.router = express.Router();
-    }
-
-    public routes(): Router{
-        this.router.post('/signup', SignUp.prototype.create);
-        this.router.post('/signin', SignIn.prototype.read);
-
-        return this.router;
-    }
-
-    public signoutRoute(): Router{
-      this.router.get('/signout', SignOut.prototype.update);
-
-      return this.router;
+  constructor() {
+    this.router = express.Router();
   }
-};
+
+  public routes(): Router {
+    this.router.post('/signup', SignUp.prototype.create);
+
+    return this.router;
+  }
+
+  public signinRoute(): Router {
+    this.router.post('/signin', SignIn.prototype.read);
+
+    return this.router;
+  }
+  public signoutRoute(): Router {
+    this.router.get('/signout', SignOut.prototype.update);
+
+    return this.router;
+  }
+}
 export const authRoutes: AuthRoutes = new AuthRoutes();
