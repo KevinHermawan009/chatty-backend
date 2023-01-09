97e0e09103bd287aeb0056edf33af61409879230
kevinhermawan009 <reactjugger@gmail.com>
2023-01-09 14:58:28 +0700

    Prettier code

--- src/features/auth/controllers/signoutController.ts
+++ src/features/auth/controllers/signoutController.ts
@@ -1,9 +1,9 @@
 import HTTP_STATUS from 'http-status-codes';
 import { Request, Response } from 'express';
 
-export class SignOut{
-  public async update(req: Request, res: Response): Promise<void>{
-    req.session =  null; //to let know cookies signout
-    res.status(HTTP_STATUS.OK).json({message: 'logout success', user: {}, token:''});
+export class SignOut {
+  public async update(req: Request, res: Response): Promise<void> {
+    req.session = null; //to let know cookies signout
+    res.status(HTTP_STATUS.OK).json({ message: 'logout success', user: {}, token: '' });
   }
 }
